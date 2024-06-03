237428a4c0d214c986312904ee3993efd09ab6fd
Julien Barnier <julien.barnier@cnrs.fr>
2024-06-03 17:24:27 +0200

    Use dotenv for postgres credentials

--- tree/config.py
+++ tree/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
+from dotenv import dotenv_values
 
-# TODO : update
 BUILD_DIRECTORY = Path.home() / ("build_results")
 TAXO_DIRECTORY = BUILD_DIRECTORY / "taxo"
 GENOMES_DIRECTORY = BUILD_DIRECTORY / "genomes"
@@ -8,10 +8,12 @@ GENOMES_DIRECTORY = BUILD_DIRECTORY / "genomes"
 # LMDATA_DIRECTORY = "/var/www/html/data"
 LMDATA_DIRECTORY = BUILD_DIRECTORY / "lmdata"
 
+config = dotenv_values(".env")
+
 DB_HOST = "localhost"
-DB_NAME = "tree"
-DB_USER = "lm"
-DB_PASSWD = "gvC5b78Ch9nDePjF"
+DB_NAME = config["PG_DB"]
+DB_USER = config["PG_USER"]
+DB_PASSWD = config["PG_PASSWD"]
 
 PSYCOPG_CONNECT_URL = (
     f"dbname='{DB_NAME}' user='{DB_USER}' host='{DB_HOST}' password='{DB_PASSWD}'"
